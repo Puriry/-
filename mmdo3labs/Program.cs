c9289bc1a642859fdfbddd0a0e5392cd814c0289
Nastya <anastasiia.rybakina@nure.ua>
2016-12-28 00:41:43 +0200

    Добавьте файлы проекта.

---
+++ mmdo3labs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mmdo3labs
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
