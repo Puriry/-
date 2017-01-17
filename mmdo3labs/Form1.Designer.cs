62613d1c203887d1ef7234c6806ff34b92ade87a
Nastya <anastasiia.rybakina@nure.ua>
2017-01-17 15:46:54 +0200

    вот так тебе

--- mmdo3labs/Form1.Designer.cs
+++ mmdo3labs/Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Обязательная переменная конструктора.блаблаблабла
+        /// Обязательная переменная конструктора.блаблаблабла   Вот так тебе=Р
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
